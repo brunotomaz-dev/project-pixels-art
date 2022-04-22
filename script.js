2eb2eeb93412f7a9d2c22de38cca5abf30c7961e
Bruno Tomaz <tomaz.bruno@gmail.com>
2022-04-22 17:13:47 -0300

    Requisito 4 e 5 ok :rocket:

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
-let pixelBoard = document.querySelectorAll('#pixel-board');
-
+const pixelBoard = document.getElementById('pixel-board');
+let pixelCreate;
 // Criar loop para fazer 25 quadrados, usando create element e depois inserir classe pixel e usar apend child para colocar  em pixel board.
+
+for (let i = 0; i < 25; i += 1) {
+  pixelCreate = document.createElement('div');
+  pixelCreate.className = 'pixel';
+  pixelBoard.appendChild(pixelCreate);
+}
