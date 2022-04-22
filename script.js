bdf537c020607b1ecff431feeb6eecaa8c1be08c
Bruno Tomaz <tomaz.bruno@gmail.com>
2022-04-22 18:58:11 -0300

    Requisito em andamento

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ const secondColor = document.getElementById('palette2');
 const thirdColor = document.getElementById('palette3');
 const lastColor = document.getElementById('palette4');
 let pixelCreate;
-// Criar loop para fazer 25 quadrados, usando create element e depois inserir classe pixel e usar apend child para colocar  em pixel board.
+const selectedColor = document.querySelector('.selected');
 
+// Cria 25 divs com class pixel
 for (let i = 0; i < 25; i += 1) {
   pixelCreate = document.createElement('div');
   pixelCreate.className = 'pixel';
   pixelBoard.appendChild(pixelCreate);
 }
 
+// função para selecionar cor
 function selectColor(event) {
   firstColor.classList.remove('selected');
   secondColor.classList.remove('selected');
@@ -20,6 +22,19 @@ function selectColor(event) {
   event.target.classList.add('selected');
 }
 
+// insere eventListener em toda class color
 document.querySelectorAll('.color').forEach(item => {
   item.addEventListener('click', selectColor);
+});
+
+// função para trocar cor (ainda não funciona)
+function changeColor(event) {
+  const cssObj = window.getComputedStyle(selectedColor, null);
+  const bgColor = cssObj.getPropertyValue('background-color');
+  event.target.classList.add('pixelColor');
+  document.querySelectorAll('.pixelColor').style.backgroundColor = bgColor;
+}
+
+document.querySelectorAll('.pixel').forEach(item => {
+  item.addEventListener('click', changeColor);
 });
