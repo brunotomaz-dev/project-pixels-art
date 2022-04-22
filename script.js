08653facd837b408dc449de53ccc64c333b3da73
Bruno Tomaz <tomaz.bruno@gmail.com>
2022-04-22 17:46:58 -0300

    Requisito 6 e 7 ok :rocket:

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const pixelBoard = document.getElementById('pixel-board');
+const firstColor = document.getElementById('palette1');
+const secondColor = document.getElementById('palette2');
+const thirdColor = document.getElementById('palette3');
+const lastColor = document.getElementById('palette4');
 let pixelCreate;
 // Criar loop para fazer 25 quadrados, usando create element e depois inserir classe pixel e usar apend child para colocar  em pixel board.
 
@@ -7,3 +11,15 @@ for (let i = 0; i < 25; i += 1) {
   pixelCreate.className = 'pixel';
   pixelBoard.appendChild(pixelCreate);
 }
+
+function selectColor(event) {
+  firstColor.classList.remove('selected');
+  secondColor.classList.remove('selected');
+  thirdColor.classList.remove('selected');
+  lastColor.classList.remove('selected');
+  event.target.classList.add('selected');
+}
+
+document.querySelectorAll('.color').forEach(item => {
+  item.addEventListener('click', selectColor);
+});
